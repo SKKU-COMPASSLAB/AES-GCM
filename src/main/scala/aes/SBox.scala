5c825d15c17a3a7aeac37319f1c1c13a1e6bc630
talha-ahmed-1 <talhaahmedashraf@gmail.com>
2025-01-17 14:36:09 +0900

    sbox implementation updated

--- src/main/scala/aes/SBox.scala
+++ src/main/scala/aes/SBox.scala
@@ -3,36 +3,44 @@ package aes
 import chisel3._
 import chisel3.util._
 
-class SBox extends Module {
-    val io = IO(new Bundle {
-      val stateIn = Input(Vec(4, Vec(4, UInt(8.W))))
-      val stateOut = Output(Vec(4, Vec(4, UInt(8.W))))
-    })
+object SBox {
+    def apply(byte: UInt): UInt = {
+      val sBox = Wire(Vec(256, UInt(8.W)))
 
-    // S-box lookup table (256 entries as per AES spec)
-    val sBox = VecInit(Seq(
-      "h63".U(8.W), "h7C".U(8.W), "h77".U(8.W), "h7B".U(8.W), "hF2".U(8.W), "h6B".U(8.W), "h6F".U(8.W), "hC5".U(8.W), "h30".U(8.W), "h01".U(8.W), "h67".U(8.W), "h2B".U(8.W), "hFE".U(8.W), "hD7".U(8.W), "hAB".U(8.W), "h76".U(8.W),
-      "hCA".U(8.W), "h82".U(8.W), "hC9".U(8.W), "h7D".U(8.W), "hFA".U(8.W), "h59".U(8.W), "h47".U(8.W), "hF0".U(8.W), "hAD".U(8.W), "hD4".U(8.W), "hA2".U(8.W), "hAF".U(8.W), "h9C".U(8.W), "hA4".U(8.W), "h72".U(8.W), "hC0".U(8.W),
-      "hB7".U(8.W), "hFD".U(8.W), "h93".U(8.W), "h26".U(8.W), "h36".U(8.W), "h3F".U(8.W), "hF7".U(8.W), "hCC".U(8.W), "h34".U(8.W), "hA5".U(8.W), "hE5".U(8.W), "hF1".U(8.W), "h71".U(8.W), "hD8".U(8.W), "h31".U(8.W), "h15".U(8.W),
-      "h04".U(8.W), "hC7".U(8.W), "h23".U(8.W), "hC3".U(8.W), "h18".U(8.W), "h96".U(8.W), "h05".U(8.W), "h9A".U(8.W), "h07".U(8.W), "h12".U(8.W), "h80".U(8.W), "hE2".U(8.W), "hEB".U(8.W), "h27".U(8.W), "hB2".U(8.W), "h75".U(8.W),
-      "h09".U(8.W), "h83".U(8.W), "h2C".U(8.W), "h1A".U(8.W), "h1B".U(8.W), "h6E".U(8.W), "h5A".U(8.W), "hA0".U(8.W), "h52".U(8.W), "h3B".U(8.W), "hD6".U(8.W), "hB3".U(8.W), "h29".U(8.W), "hE3".U(8.W), "h2F".U(8.W), "h84".U(8.W),
-      "h53".U(8.W), "hD1".U(8.W), "h00".U(8.W), "hED".U(8.W), "h20".U(8.W), "hFC".U(8.W), "hB1".U(8.W), "h5B".U(8.W), "h6A".U(8.W), "hCB".U(8.W), "hBE".U(8.W), "h39".U(8.W), "h4A".U(8.W), "h4C".U(8.W), "h58".U(8.W), "hCF".U(8.W),
-      "hD0".U(8.W), "hEF".U(8.W), "hAA".U(8.W), "hFB".U(8.W), "h43".U(8.W), "h4D".U(8.W), "h33".U(8.W), "h85".U(8.W), "h45".U(8.W), "hF9".U(8.W), "h02".U(8.W), "h7F".U(8.W), "h50".U(8.W), "h3C".U(8.W), "h9F".U(8.W), "hA8".U(8.W),
-      "h51".U(8.W), "hA3".U(8.W), "h40".U(8.W), "h8F".U(8.W), "h92".U(8.W), "h9D".U(8.W), "h38".U(8.W), "hF5".U(8.W), "hBC".U(8.W), "hB6".U(8.W), "hDA".U(8.W), "h21".U(8.W), "h10".U(8.W), "hFF".U(8.W), "hF3".U(8.W), "hD2".U(8.W),
-      "hCD".U(8.W), "h0C".U(8.W), "h13".U(8.W), "hEC".U(8.W), "h5F".U(8.W), "h97".U(8.W), "h44".U(8.W), "h17".U(8.W), "hC4".U(8.W), "hA7".U(8.W), "h7E".U(8.W), "h3D".U(8.W), "h64".U(8.W), "h5D".U(8.W), "h19".U(8.W), "h73".U(8.W),
-      "h60".U(8.W), "h81".U(8.W), "h4F".U(8.W), "hDC".U(8.W), "h22".U(8.W), "h2A".U(8.W), "h90".U(8.W), "h88".U(8.W), "h46".U(8.W), "hEE".U(8.W), "hB8".U(8.W), "h14".U(8.W), "hDE".U(8.W), "h5E".U(8.W), "h0B".U(8.W), "hDB".U(8.W),
-      "hE0".U(8.W), "h32".U(8.W), "h3A".U(8.W), "h0A".U(8.W), "h49".U(8.W), "h06".U(8.W), "h24".U(8.W), "h5C".U(8.W), "hC2".U(8.W), "hD3".U(8.W), "hAC".U(8.W), "h62".U(8.W), "h91".U(8.W), "h95".U(8.W), "hE4".U(8.W), "h79".U(8.W),
-      "hE7".U(8.W), "hC8".U(8.W), "h37".U(8.W), "h6D".U(8.W), "h8D".U(8.W), "hD5".U(8.W), "h4E".U(8.W), "hA9".U(8.W), "h6C".U(8.W), "h56".U(8.W), "hF4".U(8.W), "hEA".U(8.W), "h65".U(8.W), "h7A".U(8.W), "hAE".U(8.W), "h08".U(8.W),
-      "hBA".U(8.W), "h78".U(8.W), "h25".U(8.W), "h2E".U(8.W), "h1C".U(8.W), "hA6".U(8.W), "hB4".U(8.W), "hC6".U(8.W), "hE8".U(8.W), "hDD".U(8.W), "h74".U(8.W), "h1F".U(8.W), "h4B".U(8.W), "hBD".U(8.W), "h8B".U(8.W), "h8A".U(8.W),
-      "h70".U(8.W), "h3E".U(8.W), "hB5".U(8.W), "h66".U(8.W), "h48".U(8.W), "h03".U(8.W), "hF6".U(8.W), "h0E".U(8.W), "h61".U(8.W), "h35".U(8.W), "h57".U(8.W), "hB9".U(8.W), "h86".U(8.W), "hC1".U(8.W), "h1D".U(8.W), "h9E".U(8.W),
-      "hE1".U(8.W), "hF8".U(8.W), "h98".U(8.W), "h11".U(8.W), "h69".U(8.W), "hD9".U(8.W), "h8E".U(8.W), "h94".U(8.W), "h9B".U(8.W), "h1E".U(8.W), "h87".U(8.W), "hE9".U(8.W), "hCE".U(8.W), "h55".U(8.W), "h28".U(8.W), "hDF".U(8.W),
-      "h8C".U(8.W), "hA1".U(8.W), "h89".U(8.W), "h0D".U(8.W), "hBF".U(8.W), "hE6".U(8.W), "h42".U(8.W), "h68".U(8.W), "h41".U(8.W), "h99".U(8.W), "h2D".U(8.W), "h0F".U(8.W), "hB0".U(8.W), "h54".U(8.W), "hBB".U(8.W), "h16".U(8.W)
-    ))
+      sBox(0)  := "h63".U; sBox(1)    := "h7C".U; sBox(2)   := "h77".U; sBox(3)   := "h7B".U; sBox(4)   := "hF2".U; sBox(5)   := "h6B".U; sBox(6)   := "h6F".U; sBox(7)   := "hC5".U
+      sBox(8)  := "h30".U; sBox(9)    := "h01".U; sBox(10)  := "h67".U; sBox(11)  := "h2B".U; sBox(12)  := "hFE".U; sBox(13)  := "hD7".U; sBox(14)  := "hAB".U; sBox(15)  := "h76".U
+      sBox(16) := "hCA".U; sBox(17)   := "h82".U; sBox(18)  := "hC9".U; sBox(19)  := "h7D".U; sBox(20)  := "hFA".U; sBox(21)  := "h59".U; sBox(22)  := "h47".U; sBox(23)  := "hF0".U
+      sBox(24) := "hAD".U; sBox(25)   := "hD4".U; sBox(26)  := "hA2".U; sBox(27)  := "hAF".U; sBox(28)  := "h9C".U; sBox(29)  := "hA4".U; sBox(30)  := "h72".U; sBox(31)  := "hC0".U
+      sBox(32) := "hB7".U; sBox(33)   := "hFD".U; sBox(34)  := "h93".U; sBox(35)  := "h26".U; sBox(36)  := "h36".U; sBox(37)  := "h3F".U; sBox(38)  := "hF7".U; sBox(39)  := "hCC".U
+      sBox(40) := "h34".U; sBox(41)   := "hA5".U; sBox(42)  := "hE5".U; sBox(43)  := "hF1".U; sBox(44)  := "h71".U; sBox(45)  := "hD8".U; sBox(46)  := "h31".U; sBox(47)  := "h15".U
+      sBox(48) := "h04".U; sBox(49)   := "hC7".U; sBox(50)  := "h23".U; sBox(51)  := "hC3".U; sBox(52)  := "h18".U; sBox(53)  := "h96".U; sBox(54)  := "h05".U; sBox(55)  := "h9A".U
+      sBox(56) := "h07".U; sBox(57)   := "h12".U; sBox(58)  := "h80".U; sBox(59)  := "hE2".U; sBox(60)  := "hEB".U; sBox(61)  := "h27".U; sBox(62)  := "hB2".U; sBox(63)  := "h75".U
+      sBox(64) := "h09".U; sBox(65)   := "h83".U; sBox(66)  := "h2C".U; sBox(67)  := "h1A".U; sBox(68)  := "h1B".U; sBox(69)  := "h6E".U; sBox(70)  := "h5A".U; sBox(71)  := "hA0".U
+      sBox(72) := "h52".U; sBox(73)   := "h3B".U; sBox(74)  := "hD6".U; sBox(75)  := "hB3".U; sBox(76)  := "h29".U; sBox(77)  := "hE3".U; sBox(78)  := "h2F".U; sBox(79)  := "h84".U
+      sBox(80) := "h53".U; sBox(81)   := "hD1".U; sBox(82)  := "h00".U; sBox(83)  := "hED".U; sBox(84)  := "h20".U; sBox(85)  := "hFC".U; sBox(86)  := "hB1".U; sBox(87)  := "h5B".U
+      sBox(88) := "h6A".U; sBox(89)   := "hCB".U; sBox(90)  := "hBE".U; sBox(91)  := "h39".U; sBox(92)  := "h4A".U; sBox(93)  := "h4C".U; sBox(94)  := "h58".U; sBox(95)  := "hCF".U
+      sBox(96) := "hD0".U; sBox(97)   := "hEF".U; sBox(98)  := "hAA".U; sBox(99)  := "hFB".U; sBox(100) := "h43".U; sBox(101) := "h4D".U; sBox(102) := "h33".U; sBox(103) := "h85".U
+      sBox(104) := "h45".U; sBox(105) := "hF9".U; sBox(106) := "h02".U; sBox(107) := "h7F".U; sBox(108) := "h50".U; sBox(109) := "h3C".U; sBox(110) := "h9F".U; sBox(111) := "hA8".U
+      sBox(112) := "h51".U; sBox(113) := "hA3".U; sBox(114) := "h40".U; sBox(115) := "h8F".U; sBox(116) := "h92".U; sBox(117) := "h9D".U; sBox(118) := "h38".U; sBox(119) := "hF5".U
+      sBox(120) := "hBC".U; sBox(121) := "hB6".U; sBox(122) := "hDA".U; sBox(123) := "h21".U; sBox(124) := "h10".U; sBox(125) := "hFF".U; sBox(126) := "hF3".U; sBox(127) := "hD2".U
+      sBox(128) := "hCD".U; sBox(129) := "h0C".U; sBox(130) := "h13".U; sBox(131) := "hEC".U; sBox(132) := "h5F".U; sBox(133) := "h97".U; sBox(134) := "h44".U; sBox(135) := "h17".U
+      sBox(136) := "hC4".U; sBox(137) := "hA7".U; sBox(138) := "h7E".U; sBox(139) := "h3D".U; sBox(140) := "h64".U; sBox(141) := "h5D".U; sBox(142) := "h19".U; sBox(143) := "h73".U
+      sBox(144) := "h60".U; sBox(145) := "h81".U; sBox(146) := "h4F".U; sBox(147) := "hDC".U; sBox(148) := "h22".U; sBox(149) := "h2A".U; sBox(150) := "h90".U; sBox(151) := "h88".U
+      sBox(152) := "h46".U; sBox(153) := "hEE".U; sBox(154) := "hB8".U; sBox(155) := "h14".U; sBox(156) := "hDE".U; sBox(157) := "h5E".U; sBox(158) := "h0B".U; sBox(159) := "hDB".U
+      sBox(160) := "hE0".U; sBox(161) := "h32".U; sBox(162) := "h3A".U; sBox(163) := "h0A".U; sBox(164) := "h49".U; sBox(165) := "h06".U; sBox(166) := "h24".U; sBox(167) := "h5C".U
+      sBox(168) := "hC2".U; sBox(169) := "hD3".U; sBox(170) := "hAC".U; sBox(171) := "h62".U; sBox(172) := "h91".U; sBox(173) := "h95".U; sBox(174) := "hE4".U; sBox(175) := "h79".U
+      sBox(176) := "hE7".U; sBox(177) := "hC8".U; sBox(178) := "h37".U; sBox(179) := "h6D".U; sBox(180) := "h8D".U; sBox(181) := "hD5".U; sBox(182) := "h4E".U; sBox(183) := "hA9".U
+      sBox(184) := "h6C".U; sBox(185) := "h56".U; sBox(186) := "hF4".U; sBox(187) := "hEA".U; sBox(188) := "h65".U; sBox(189) := "h7A".U; sBox(190) := "hAE".U; sBox(191) := "h08".U
+      sBox(192) := "hBA".U; sBox(193) := "h78".U; sBox(194) := "h25".U; sBox(195) := "h2E".U; sBox(196) := "h1C".U; sBox(197) := "hA6".U; sBox(198) := "hB4".U; sBox(199) := "hC6".U
+      sBox(200) := "hE8".U; sBox(201) := "hDD".U; sBox(202) := "h74".U; sBox(203) := "h1F".U; sBox(204) := "h4B".U; sBox(205) := "hBD".U; sBox(206) := "h8B".U; sBox(207) := "h8A".U
+      sBox(208) := "h70".U; sBox(209) := "h3E".U; sBox(210) := "hB5".U; sBox(211) := "h66".U; sBox(212) := "h48".U; sBox(213) := "h03".U; sBox(214) := "hF6".U; sBox(215) := "h0E".U
+      sBox(216) := "h61".U; sBox(217) := "h35".U; sBox(218) := "h57".U; sBox(219) := "hB9".U; sBox(220) := "h86".U; sBox(221) := "hC1".U; sBox(222) := "h1D".U; sBox(223) := "h9E".U
+      sBox(224) := "hE1".U; sBox(225) := "hF8".U; sBox(226) := "h98".U; sBox(227) := "h11".U; sBox(228) := "h69".U; sBox(229) := "hD9".U; sBox(230) := "h8E".U; sBox(231) := "h94".U
+      sBox(232) := "h9B".U; sBox(233) := "h1E".U; sBox(234) := "h87".U; sBox(235) := "hE9".U; sBox(236) := "hCE".U; sBox(237) := "h55".U; sBox(238) := "h28".U; sBox(239) := "hDF".U
+      sBox(240) := "h8C".U; sBox(241) := "hA1".U; sBox(242) := "h89".U; sBox(243) := "h0D".U; sBox(244) := "hBF".U; sBox(245) := "hE6".U; sBox(246) := "h42".U; sBox(247) := "h68".U
+      sBox(248) := "h41".U; sBox(249) := "h99".U; sBox(250) := "h2D".U; sBox(251) := "h0F".U; sBox(252) := "hB0".U; sBox(253) := "h54".U; sBox(254) := "hBB".U; sBox(255) := "h16".U
 
-    for (i <- 0 until 4) {
-      for (j <- 0 until 4) {
-        io.stateOut(i)(j) := sBox(io.stateIn(i)(j))
-      }
+      sBox(byte)
     }
 }
 
--- src/main/scala/aes/SubBytes.scala
+++ src/main/scala/aes/SubBytes.scala
@@ -3,6 +3,8 @@ package aes
 import chisel3._
 import chisel3.util._
 
+import aes.SBox
+
 class SubBytes extends Module{
     val io = IO(new Bundle{
         val stateIn = Input(Vec(4, Vec(4, UInt(8.W))))
@@ -10,12 +12,10 @@ class SubBytes extends Module{
     })
 
     val substitutedBytes = Wire(Vec(4, Vec(4, UInt(8.W))))
-    val sBox = Module(new SBox)
 
     for (i <- 0 until 4) {
         for (j <- 0 until 4) {
-            sBox.io.stateIn(i)(j) := io.stateIn(i)(j)
-            substitutedBytes(i)(j) := sBox.io.stateOut(i)(j) // sBox(io.stateIn(i)(j)) // sBox.io.byteOut(i)(j)
+            substitutedBytes(i)(j) := SBox(io.stateIn(i)(j))
         }
     }
 
