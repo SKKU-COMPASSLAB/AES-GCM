b11579c6adb6d6df2ce3a247db7c8f6554a51791
talha-ahmed-1 <talhaahmedashraf@gmail.com>
2025-01-17 15:58:02 +0900

    test file updated

--- src/test/scala/aes/AESTest.scala
+++ src/test/scala/aes/AESTest.scala
@@ -50,32 +50,60 @@ class AESTest extends AnyFlatSpec {
   //   }
   // }
 
-  // it should "test SubBytes" in {
-  //   simulate(new SubBytes) { dut =>
-  //     dut.io.stateIn(0)(1).poke(1.U)
-    //   dut.io.stateIn(1).poke(2.U)
-    //   dut.io.stateIn(2).poke(3.U)
-    //   dut.io.stateIn(3).poke(4.U)
-    //   dut.io.stateIn(4).poke(5.U)
-    //   dut.io.stateIn(5).poke(6.U)
-    //   dut.io.stateIn(6).poke(7.U)
-    //   dut.io.stateIn(7).poke(8.U)
-    //   dut.io.stateIn(8).poke(9.U)
-    //   dut.io.stateIn(9).poke(10.U)
-    //   dut.io.stateIn(10).poke(11.U)
-    //   dut.io.stateIn(11).poke(12.U)
-    //   dut.io.stateIn(12).poke(13.U)
-    //   dut.io.stateIn(13).poke(14.U)
-    //   dut.io.stateIn(14).poke(15.U)
-    //   dut.io.stateIn(15).poke(16.U)
-    //   dut.clock.step(10)
-    // }
+    // it should "test SubBytes" in {
+    //   simulate(new SubBytes) { dut =>
+    //     dut.io.stateIn(0)(1).poke(1.U)
+    //     // dut.io.stateIn(1).poke(2.U)
+    //     // dut.io.stateIn(2).poke(3.U)
+    //     // dut.io.stateIn(3).poke(4.U)
+    //     // dut.io.stateIn(4).poke(5.U)
+    //     // dut.io.stateIn(5).poke(6.U)
+    //     // dut.io.stateIn(6).poke(7.U)
+    //     // dut.io.stateIn(7).poke(8.U)
+    //     // dut.io.stateIn(8).poke(9.U)
+    //     // dut.io.stateIn(9).poke(10.U)
+    //     // dut.io.stateIn(10).poke(11.U)
+    //     // dut.io.stateIn(11).poke(12.U)
+    //     // dut.io.stateIn(12).poke(13.U)
+    //     // dut.io.stateIn(13).poke(14.U)
+    //     // dut.io.stateIn(14).poke(15.U)
+    //     // dut.io.stateIn(15).poke(16.U)
+    //     dut.io.stateOut(0)(1).expect(0x63.U)
+    //     dut.clock.step(10)
+    //   }
     // }
 
-    it should "test Mix Columns" in {
-    simulate(new MixColumns) { dut =>
-        // dut.io.byteIn.poke(1.U)
-        dut.clock.step(10)
-      }
+    // it should "test SubBytes" in {
+    //   simulate(new SubBytes) { dut =>
+    //   dut.clock.step(10)
+    //       }
+    // }
+
+    // it should "test Mix Columns" in {
+    // simulate(new MixColumns) { dut =>
+    //     // dut.io.byteIn.poke(1.U)
+    //     dut.clock.step(10)
+    //   }
+    // }
+
+    // it should "test Key Expansion" in {
+    // simulate(new KeyExpansion) { dut =>
+    //     // dut.io.byteIn.poke(1.U)
+    //     dut.clock.step(10)
+    //   }
+    // }
+
+    // it should "test new implementation of SBox" in {
+    // simulate(new SBoxO) { dut =>
+    //     dut.io.stateIn.poke(0.U)
+    //     dut.io.stateOut.expect("h63".U)
+    //     dut.clock.step(10)
+    //   }
+    // }
+
+    it should "test Key Expansion" in {
+      simulate(new KeyExpansion) { dut =>
+      dut.clock.step(10)
+          }
     }
 }
