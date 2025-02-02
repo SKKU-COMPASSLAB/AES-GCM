509fd37b142426fa706d0fbef6d6462c87b7ac38
talha-ahmed-1 <talhaahmedashraf@gmail.com>
2025-02-03 05:00:42 +0900

    gfmult(2^128) calculation upadted

---
+++ src/main/scala/gcm/GFMult.scala
@@ -0,0 +1,56 @@
+package gcm
+
+import chisel3._
+import chisel3.util._
+
+class GFMult extends Module{
+    val io = IO(new Bundle{
+        val a = Input(UInt(128.W))
+        val b = Input(UInt(128.W))
+        val out = Output(UInt(128.W))
+    })
+
+    val a = WireInit(Reverse(io.a))
+    val b = WireInit(Reverse(io.b))
+    val r = Cat("b11100001".U, 0.U(120.W))
+
+    val z = Reg(UInt(128.W))
+    val v = Reg(UInt(128.W))
+
+    val counter = Reg(UInt(7.W))
+
+    val sIDLE :: sCOMPUTE :: sFINISH :: Nil = Enum(3)
+    val state = RegInit(sIDLE)
+
+    io.out := DontCare
+
+    switch(state){
+        is(sIDLE){
+            z := 0.U
+            v := b
+            counter := 0.U
+            state := sCOMPUTE
+        }
+        is(sCOMPUTE){
+            when(a(counter)){
+                z := z ^ v
+            }
+            when(v(0)){
+                v := (v >> 1) ^ r
+            }
+            .otherwise{
+                v := v >> 1
+            }
+
+            counter := counter + 1.U
+            when(counter.andR){
+                state := sFINISH
+            }
+        }
+        is(sFINISH){
+            io.out := Reverse(z)
+            state := sIDLE
+        }
+    }
+
+}
