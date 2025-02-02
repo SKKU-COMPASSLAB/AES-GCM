4a03307dbf78862b290760eb41072602ef5a63c6
talha-ahmed-1 <talhaahmedashraf@gmail.com>
2025-02-03 06:01:31 +0900

    addes hash_subkey in ghash calc.

---
+++ src/main/scala/gcm/GHASH.scala
@@ -0,0 +1,22 @@
+package gcm
+
+import chisel3._
+import chisel3.util._
+
+class GHASH extends Module{
+    val io = IO(new Bundle{
+        val xIn = Input(UInt(128.W))
+        val cypherIn = Input(UInt(128.W))
+        val hashKey = Input(UInt(128.W))
+        val ghash = Output(UInt(128.W))
+    })
+
+    val gfmult = Module(new GFMult)
+
+    val xor_res = io.xIn ^ io.cypherIn
+
+    gfmult.io.a := xor_res
+    gfmult.io.b := hashKey
+
+    io.ghash := gfmult.io.out
+}
